baffec392a93a0b8c583000ae894d84d6d8e9e04
oleyon <subbotin-2000@mail.ru>
2022-03-10 00:20:27 +0300

    a

---
+++ GenericPattern/GraphExplorer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPattern
+{
+    internal abstract class GraphExplorer
+    {
+        public void Search()
+        {
+
+        }
+        public abstract void StartOfTheCrawl();
+        public abstract void EndOfTheTour();
+        public abstract void VisitNodeV();
+        public abstract void VisitRibE();
+    }
+}
