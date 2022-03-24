909dd50b69529a2f9f7c682fb3b90ad9a747ca6a
oleyon <subbotin-2000@mail.ru>
2022-03-24 07:09:47 +0300

    a

--- CommandPattern/Program.cs
+++ CommandPattern/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CommandPattern;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
